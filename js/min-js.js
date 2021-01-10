34ee5649fd266f1eb0ee118affc18100116e8bc4
yuugurekun <76441933+yuugurekun@users.noreply.github.com>
2021-01-10 19:07:02 +0900

    1st commit.

---
+++ js/min-js.js
@@ -0,0 +1,21 @@
+$(function(){
+    function animation(){
+        $('.fadeInUp').each(function(){
+        //ターゲットの位置を取得
+        var target = $(this).offset().top;
+        //スクロール量を取得
+        var scroll = $(window).scrollTop();
+        //ウィンドウの高さを取得
+        var windowHeight = $(window).height();
+        //ターゲットまでスクロールするとフェードインする
+        if (scroll > target - windowHeight){
+            $(this).css('opacity','1');
+            $(this).css('transform','translateY(0)');
+        }
+        });
+    }
+    animation();
+    $(window).scroll(function (){
+        animation();
+    });
+});
